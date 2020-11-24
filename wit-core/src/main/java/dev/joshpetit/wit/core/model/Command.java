aa29fe48878f9f29bf5314125bea8667940e90da
joshu <joshuapetitma@yahoo.com>
2020-11-24 13:25:33 -0500

    feat: adds enum for system commands
    System will have a list of enums which depict actions to do such as
    turning on capslock, pressing shift pressing the home or end buttons
    possibly.

--- wit-core/src/main/java/dev/joshpetit/wit/core/model/Command.java
+++ wit-core/src/main/java/dev/joshpetit/wit/core/model/Command.java
@@ -1,11 +1,17 @@
 package dev.joshpetit.wit.core.model;
 
 public abstract class Command {
+
 	public static class TYPE {
 		public static int SYSTEM = 0;
 		public static int APPEND = 1;
 		public static int DELETE = 2;
 	}
+
+	public static enum SYSTEM {
+		CAPS_LOCK;
+	}
+
 	private int type;
 
 	public Command(int type) {
