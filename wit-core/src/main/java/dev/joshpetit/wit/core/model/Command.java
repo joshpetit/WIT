89988581acb24172cbe9efa27fcdf3e478330867
joshu <joshuapetitma@yahoo.com>
2020-11-24 09:58:05 -0500

    refactor: makes Command an abstract class

--- wit-core/src/main/java/dev/joshpetit/wit/core/model/Command.java
+++ wit-core/src/main/java/dev/joshpetit/wit/core/model/Command.java
@@ -1,34 +1,19 @@
 package dev.joshpetit.wit.core.model;
 
-public class Command {
+public abstract class Command {
 	public static class TYPE {
 		public static int SYSTEM = 0;
 		public static int APPEND = 1;
 		public static int DELETE = 2;
 	}
 	private int type;
-	private String payload;
-
-	public Command(int type, String payload) {
-		this.type = type;
-		this.payload = payload;
-	}
 
 	public Command(int type) {
 		this.type = type;
-		this.payload = payload;
-	}
-
-	public boolean containsPayload() {
-		return payload != null;
 	}
 
 	public int getType() {
 		return this.type;
-	}
-
-	public String getPayload() {
-		return this.payload;
 	}
 
 }
