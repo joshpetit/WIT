e1bf2acb06f8735e5df262e423c9af96042e9942
joshu <joshuapetitma@yahoo.com>
2020-11-29 10:51:29 -0500

    implements basic String Typing system

--- wit-core/src/main/java/dev/joshpetit/wit/core/interpret/StringSystem.java
+++ wit-core/src/main/java/dev/joshpetit/wit/core/interpret/StringSystem.java
@@ -4,21 +4,35 @@ import dev.joshpetit.wit.core.model.*;
 import java.util.Properties;
 
 public class StringSystem extends TypingSystem {
+	private StringBuilder text;
 
 	public StringSystem(Properties config) {
 		super(config);
+		text = new StringBuilder();
 	}
 
 	public void appendCommand(AppendCommand com) {
-		System.out.println("About to Append something!");
+		if (capslock) {
+			this.text.append(com.getUpper());
+		} else {
+			this.text.append(com.getLower());
+		}
+	}
+
+	public String getText() {
+		return text.toString();
 	}
 
 	public void systemCommand(SystemCommand com) {
-		System.out.println("About to System something!");
+		if (com.getCommand() == Command.SYSTEM.CAPS_LOCK) {
+			this.capslock = !this.capslock;
+		}
 	}
 
 	public void deleteCommand(DeleteCommand com) {
-		System.out.println("About to Delete something!");
+		if (com.getCommand() == Command.DELETE.CHAR) {
+			text.deleteCharAt(text.length() -1);
+		}
 	}
 
 }
