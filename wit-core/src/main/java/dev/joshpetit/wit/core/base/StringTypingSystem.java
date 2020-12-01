bbc02c0a0ee5e619f7783caf6cbc4f3c5b646218
joshu <joshuapetitma@yahoo.com>
2020-12-01 16:51:17 -0500

    (StringT.S) implements executeCommand method

--- wit-core/src/main/java/dev/joshpetit/wit/core/base/StringTypingSystem.java
+++ wit-core/src/main/java/dev/joshpetit/wit/core/base/StringTypingSystem.java
@@ -3,7 +3,7 @@ import java.util.Properties;
 import java.util.List;
 import dev.joshpetit.wit.core.commands.*;
 
-public class StringTypingSystem extends TypingSystem {
+public class StringTypingSystem extends TypingSystem<BasicCommandable> {
 	
 	public StringTypingSystem(Properties config, BasicCommandable context) {
 		super(config, context);
@@ -19,5 +19,13 @@ public class StringTypingSystem extends TypingSystem {
 	public void executeCommand(String key) {
 		String command = config.getProperty(key);
 		if (command == null) return;
+		Command parsed = TypingSystem.parseCommand(command);
+		if (parsed instanceof MessageCommand) {
+			this.context.typingMessage((MessageCommand) parsed);
+		} else if (parsed instanceof DeleteCommand) {
+			this.context.typingDelete((DeleteCommand) parsed);
+		} else if (parsed instanceof AppendCommand) {
+			this.context.typingAppend((AppendCommand) parsed);
+		}
 	}
 }
