3f09d489b2b6b85cd445b33f3c69d4d2f9706658
joshu <joshuapetitma@yahoo.com>
2020-12-02 14:15:23 -0500

    implements DeleteCommand

--- wit-core/src/main/java/dev/joshpetit/wit/core/commands/DeleteCommand.java
+++ wit-core/src/main/java/dev/joshpetit/wit/core/commands/DeleteCommand.java
@@ -1,10 +1,15 @@
 package dev.joshpetit.wit.core.commands;
 
 public class DeleteCommand extends Command {
+	private DeleteCommand.TYPE type;
 	public DeleteCommand(DeleteCommand.TYPE t) {
-
+		this.type = t;
 	}
 	public enum TYPE {
 		CHAR, WORD
+	}
+
+	public DeleteCommand.TYPE getType() {
+		return type;
 	}
 }
