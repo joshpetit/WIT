a8d5333fa58e07d3c09df2b10766fbff3128beac
joshu <joshuapetitma@yahoo.com>
2020-12-02 13:31:43 -0500

    adds needed methods to BasicCommandable for clarity

--- wit-core/src/main/java/dev/joshpetit/wit/core/commands/BasicCommandable.java
+++ wit-core/src/main/java/dev/joshpetit/wit/core/commands/BasicCommandable.java
@@ -1,4 +1,7 @@
 package dev.joshpetit.wit.core.commands;
 
 public interface BasicCommandable extends AppendCommandable, MessageCommandable, DeleteCommandable{
+	public void typingAppend(AppendCommand c);
+	public void typingDelete(DeleteCommand c);
+	public void typingMessage(MessageCommand c);
 }
