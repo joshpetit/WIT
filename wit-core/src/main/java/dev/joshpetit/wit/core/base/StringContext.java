452e3cd185983a3778cf82e6cabea0a7ded97476
joshu <joshuapetitma@yahoo.com>
2020-12-02 13:43:36 -0500

    implements typingAppend for StringContext

---
+++ wit-core/src/main/java/dev/joshpetit/wit/core/base/StringContext.java
@@ -0,0 +1,30 @@
+package dev.joshpetit.wit.core.base;
+import java.util.Properties;
+import java.util.List;
+import dev.joshpetit.wit.core.commands.*;
+
+
+public class StringContext implements BasicCommandable {
+	protected StringBuilder text;
+	boolean capsLockOn;
+
+	public StringContext() {
+		this("");
+	}
+
+	public StringContext(String current) {
+		text = new StringBuilder(current);
+	}
+
+	public void typingAppend(AppendCommand c) {
+		text.append( capsLockOn ? c.getUpper() : c.getLower());
+	}
+
+	public void typingDelete(DeleteCommand c) {
+
+	}
+
+	public void typingMessage(MessageCommand c) {
+
+	}
+}
