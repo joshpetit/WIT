7d33f5f3116a183b03cc1f94d96623bc4c748900
joshu <joshuapetitma@yahoo.com>
2020-12-02 20:22:57 -0500

    implements MessageCommand

--- wit-core/src/main/java/dev/joshpetit/wit/core/commands/MessageCommand.java
+++ wit-core/src/main/java/dev/joshpetit/wit/core/commands/MessageCommand.java
@@ -1,12 +1,17 @@
 package dev.joshpetit.wit.core.commands;
 
 public class MessageCommand extends Command {
+	private MessageCommand.TYPE type;
 	public enum TYPE {
 		CAPS_LOCK
 	}
 
-	public MessageCommand(MessageCommand.TYPE syscmd) {
+	public MessageCommand(MessageCommand.TYPE t) {
+		this.type = t;
+	}
 
+	public MessageCommand.TYPE getType() {
+		return this.type;
 	}
 
 }
