3169a8e24448217e942481a29c693e1b4b1d3b28
joshu <joshuapetitma@yahoo.com>
2020-12-03 12:04:51 -0500

    adds test for BasicTypingSystem

---
+++ wit-core/src/test/java/dev/joshpetit/wit/core/base/TypingSystemTest.java
@@ -0,0 +1,47 @@
+package dev.joshpetit.wit.core.base;
+
+import static org.junit.jupiter.api.Assertions.*;
+import dev.joshpetit.wit.core.commands.*;
+
+import java.util.Properties;
+import java.util.Arrays;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeAll;
+
+
+class SystemTest {
+
+	@Nested
+	@DisplayName("Tests for String Context")
+	class BasicTypingSystemTest {
+
+		@Test
+		void testMethodCalls() {
+			boolean[] called = new boolean[3];
+			BasicCommandable context = new BasicCommandable() {
+				public void typingMessage(MessageCommand c) {called[0] = true;}
+				public void typingDelete(DeleteCommand c) {called[1] = true;}
+				public void typingAppend(AppendCommand c) {called[2] = true;}
+			};
+			Properties props = new Properties();
+			props.setProperty("00", "00");
+			props.setProperty("01", "10");
+			props.setProperty("02", "2aA");
+			//bts lol, my sister would scream
+			BasicTypingSystem bts = new BasicTypingSystem(props, context); 
+			bts.input(0);
+			bts.input(0);
+			assertTrue(called[0], "typingMessage should have been called");
+			bts.input(0);
+			bts.input(1);
+			assertTrue(called[1], "typingDelete command should have been called");
+			bts.input(0);
+			bts.input(2);
+			assertTrue(called[2], "typingAppend command should have been called");
+		}
+
+	}
+}
