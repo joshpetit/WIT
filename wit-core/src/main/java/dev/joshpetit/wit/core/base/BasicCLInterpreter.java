8faf2b85067f73aed2d80d0281eb2cae2ae33484
joshu <joshuapetitma@yahoo.com>
2020-12-07 14:12:59 -0500

    creates basic CLI interpreter mappings generator

---
+++ wit-core/src/main/java/dev/joshpetit/wit/core/base/BasicCLInterpreter.java
@@ -0,0 +1,31 @@
+package dev.joshpetit.wit.core.base;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.Map;
+import java.util.HashMap;
+
+public class BasicCLInterpreter extends InputInterpreter {
+	protected Map<String, Integer> map;
+	private BufferedReader reader;
+
+	public BasicCLInterpreter(TypingSystem ts) {
+		super(ts);
+		reader = new BufferedReader(new InputStreamReader(System.in));
+		createMappings();
+	}
+
+	public void createMappings() {
+		map = new HashMap<>();
+		for(int i=0; i < 10; i++) {
+
+		try {
+			map.put(reader.readLine(), i);
+			System.out.println(map);
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+
+		}
+	}
+}
