9d445083ba70518c6ad64857cbba7c11b6647744
joshu <joshuapetitma@yahoo.com>
2020-12-10 13:33:34 -0500

    adds test for creating mappings for BCI

--- wit-core/src/test/java/dev/joshpetit/wit/core/base/InputInterpreterTest.java
+++ wit-core/src/test/java/dev/joshpetit/wit/core/base/InputInterpreterTest.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.io.ByteArrayInputStream;
 
 import java.util.Properties;
+import java.util.Map;
+import java.util.HashMap;
 import java.util.Arrays;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
@@ -20,18 +22,28 @@ class InputInterpreterTest {
 	class BasicCLInterpreterTest {
 
 		@Test
-		void interactive() {
+		void testMappingCreation() {
 			Properties config = new Properties();
+			Map<String, Integer> expectedMapping = new HashMap<>();
+			expectedMapping.put("a", 0);
+			expectedMapping.put("b", 1);
+			expectedMapping.put("c", 2);
+			expectedMapping.put("d", 3);
+			expectedMapping.put("e", 4);
+			expectedMapping.put("f", 5);
+			expectedMapping.put("g", 6);
+			expectedMapping.put("h", 7);
+			expectedMapping.put("i", 8);
+			expectedMapping.put("j", 9);
 			try{
-			config.load(InputInterpreter.class.getResourceAsStream("defaultConfig.properties"));
-			StringContext context = new StringContext();
-			TypingSystem ts = new BasicTypingSystem(config, context);
-			BasicCLInterpreter interpreter = new BasicCLInterpreter(ts);
-			
-			InputStream orgSysIn = System.in;
-			ByteArrayInputStream in = new ByteArrayInputStream("a\nb\nc\nd\ne\nf\ng\nh\ni\nj".getBytes());
-			interpreter.setInputStream(in);
-			interpreter.setMappings();
+				config.load(InputInterpreter.class.getResourceAsStream("defaultConfig.properties"));
+				StringContext context = new StringContext();
+				TypingSystem ts = new BasicTypingSystem(config, context);
+				BasicCLInterpreter interpreter = new BasicCLInterpreter(ts);
+				ByteArrayInputStream in = new ByteArrayInputStream("a\nb\nc\nd\ne\nf\ng\nh\ni\nj".getBytes());
+				interpreter.setInputStream(in);
+				interpreter.setMappings();
+				assertEquals(interpreter.getMappings(), expectedMapping);
 			} catch(IOException e) {
 				e.printStackTrace();
 			}
