dec9f673e49722ef74801ca916b74a9f3fb82597
joshu <joshuapetitma@yahoo.com>
2020-12-28 11:34:48 -0500

    Adds button panes 🥞

--- wit-gui/src/main/java/dev/joshpetit/wit/gui/launcher/Main.java
+++ wit-gui/src/main/java/dev/joshpetit/wit/gui/launcher/Main.java
@@ -1,4 +1,5 @@
 package dev.joshpetit.wit.gui.launcher;
+import javafx.geometry.Insets;
 
 import java.io.IOException;
 
@@ -16,9 +17,11 @@ import javafx.scene.input.KeyEvent;
 import javafx.scene.Scene;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import java.util.Properties;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -28,10 +31,25 @@ public class Main extends Application {
     Properties config;
     int index;
 
+    public Pane createButtonDisplay() {
+        HBox row = new HBox();
+        row.setSpacing(11);
+        row.setPrefHeight(200);
+        for (int i = 0; i < 11; i++) {
+            Pane p = new Pane();
+            p.setPrefWidth(200);
+            p.setPrefHeight(100);
+            p.setStyle("-fx-background-color: #ffffff;");
+            row.getChildren().add(p);
+            System.out.println("add");
+        }
+        return row;
+    }
+
     public void createInterpreter() {
         try {
             config = BasicTypingSystem.getDefaultProperties();
-            area = new CommandableTextArea("Demo");
+            area = new CommandableTextArea("Demo Text");
             //area.setDisable(true);
             area.setOpacity(1);
             ts = new BasicTypingSystem(config, area);
@@ -45,7 +63,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) {
         createInterpreter();
-        Pane root = new Pane();
+        BorderPane root = new BorderPane();
         root.setOnKeyPressed(new EventHandler<KeyEvent>() {
             @Override
             public void handle(KeyEvent e) {
@@ -57,24 +75,20 @@ public class Main extends Application {
                 }
             }
         });
-
-        HBox hbox = new HBox();
-        hbox.setAlignment(Pos.CENTER);
-        VBox vbox = new VBox();
-        vbox.setAlignment(Pos.CENTER);
-        hbox.getChildren().add(area);
-
-        vbox.getChildren().add(hbox);
-        root.getChildren().add(vbox);
-
+        Pane row = createButtonDisplay();
+        row.setStyle("-fx-background-color: #336699;");
+        row.setPadding(new Insets(15, 12, 15, 12));
+        root.setCenter(area);
+        root.setBottom(row);
         Scene scene = new Scene(root, 700, 500);
+
         primaryStage.setTitle("WIT - Blind Keyboard");
         primaryStage.setScene(scene);
         primaryStage.show();
         root.requestFocus();
     }
 
-     public static void main(String[] args) {
+    public static void main(String[] args) {
         launch(args);
     }
 }
