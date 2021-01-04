5405b366b5d42a2e6d1438ee9699f18fec52f7b3
joshu <joshuapetitma@yahoo.com>
2021-01-04 16:47:12 -0500

    updates tests

--- wit-core/src/test/java/dev/joshpetit/wit/core/base/TypingSystemTest.java
+++ wit-core/src/test/java/dev/joshpetit/wit/core/base/TypingSystemTest.java
@@ -25,10 +25,18 @@ class TypingSystemTest {
 		void testMethodCalls() {
 			boolean[] called = new boolean[3];
 			BasicCommandable context = new BasicCommandable() {
-				public void typingMessage(MessageCommand c) {called[0] = true;}
-				public void typingDelete(DeleteCommand c) {called[1] = true;}
-				public void typingAppend(AppendCommand c) {called[2] = true;}
-				public boolean nextUpper(){return true;}
+				public void typingMessage(MessageCommand c) {
+					called[0] = true;
+				}
+				public void typingDelete(DeleteCommand c) {
+					called[1] = true;
+				}
+				public void typingAppend(AppendCommand c) {
+					called[2] = true;
+				}
+				public boolean nextUpper() {
+					return true;
+				}
 				Map<String, Integer> s = new HashMap<>();
 			};
 			Properties props = new Properties();
@@ -36,7 +44,7 @@ class TypingSystemTest {
 			props.setProperty("01", "10");
 			props.setProperty("02", "2aA");
 			//bts lol, my sister would scream
-			BasicTypingSystem bts = new BasicTypingSystem(props, context); 
+			BasicTypingSystem bts = new BasicTypingSystem(props, context);
 			bts.input(0);
 			bts.input(0);
 			assertTrue(called[0], "typingMessage should have been called");
@@ -51,18 +59,75 @@ class TypingSystemTest {
 		@Test
 		void testStandardCompletions() {
 			boolean[] called = new boolean[3];
-			final boolean upper = false;
+			final boolean[] upper = new boolean[] {false};
 			BasicCommandable context = new BasicCommandable() {
 				public void typingMessage(MessageCommand c) {}
 				public void typingDelete(DeleteCommand c) {}
 				public void typingAppend(AppendCommand c) {}
-				public boolean nextUpper(){return upper;}
+				public boolean nextUpper() {
+					return upper[0];
+				}
 			};
-				Properties props = BasicTypingSystem.getDefaultProperties();
-				BasicTypingSystem bts = new BasicTypingSystem(props, context); 
-				System.out.println(bts.getStandardCompletions());
-				bts.input(9);
-				System.out.println(bts.getStandardCompletions());
+			Properties props = BasicTypingSystem.getDefaultProperties();
+			BasicTypingSystem bts = new BasicTypingSystem(props, context);
+			Map comps = Map.of(
+			                0, "a-e",
+			                1, "f-j",
+			                2, "k-o",
+			                3, "p-t",
+			                4, "u-y",
+			                5, "",
+			                6, "",
+			                7, "",
+			                8, "",
+			                9, ""
+			            );
+			assertEquals(comps, bts.getStandardCompletions());
+			bts.input(9);
+			comps = Map.of(
+			            0, "",
+			            1, "",
+			            2, "",
+			            3, "",
+			            4, "z",
+			            5, "",
+			            6, "",
+			            7, "",
+			            8, "",
+			            9, "0"
+			        );
+			assertEquals(comps, bts.getStandardCompletions());
+			bts.input(9);
+
+			upper[0] = true;
+			comps = Map.of(
+			            0, "A-E",
+			            1, "F-J",
+			            2, "K-O",
+			            3, "P-T",
+			            4, "U-Y",
+			            5, "",
+			            6, "",
+			            7, "",
+			            8, "",
+			            9, ""
+			        );
+			assertEquals(comps, bts.getStandardCompletions());
+
+			bts.input(4);
+			comps = Map.of(
+			            0, "",
+			            1, "",
+			            2, "",
+			            3, "",
+			            4, "5",
+			            5, "U",
+			            6, "V",
+			            7, "W",
+			            8, "X",
+			            9, "Y"
+			        );
+			assertEquals(comps, bts.getStandardCompletions());
 		}
 	}
 }
