68985c2052e9899f1d329e9ed7ae1b31ba4a10db
joshu <joshuapetitma@yahoo.com>
2021-01-12 07:09:48 -0500

    Improves TypingSystem documentation

--- wit-core/src/main/java/dev/joshpetit/wit/core/base/TypingSystem.java
+++ wit-core/src/main/java/dev/joshpetit/wit/core/base/TypingSystem.java
@@ -4,16 +4,16 @@ import java.util.List;
 import dev.joshpetit.wit.core.commands.*;
 
 /**
- * The typing system is the middleway to 
- * communicate with a context.
- *
+ * A TypingSystem is a class that 
+ * sends messages to a specified 
+ * {@link dev.joshpetit.wit.core.commands.Commandable}
+ * {@link TypingSystem#context}.
  * The typing system holds a context that implements
  * some level of 
- * {@link dev.joshpetit.wit.core.commands.Commandable}
- *
- * typically receives inputs from 
+ * {@link dev.joshpetit.wit.core.commands.Commandable}.
+ * The system typically receives inputs from 
  * {@link dev.joshpetit.wit.core.base.InputInterpreter}
- * that it translates into commands to {@link TypingSystem#context}
+ * that it translates into commands and sends to {@link TypingSystem#context}.
  */
 public abstract class TypingSystem<T extends Commandable> {
 
@@ -26,11 +26,15 @@ public abstract class TypingSystem<T extends Commandable> {
 	/**
 	 * The configuration that the typing
 	 * system uses to translate inputs.
+	 * Configurations are based on
+	 * key-value pairs. How the key value
+	 * pair is interpreted is based on
+	 * the specific system implementation.
 	 */
 	protected Properties config;
 
 	/**
-	 * Constructs a TypingSystem appropriately.
+	 * Constructs a TypingSystem with a configuration and context.
 	 * @param config The configuration mappings the system translates.
 	 * @param context the context that that the system
 	 * inputs comamnds to.
@@ -41,14 +45,16 @@ public abstract class TypingSystem<T extends Commandable> {
 	}
 
 	/**
-	 * Sends a numbered input to the system that it can
-	 * use to translate with other inputs.
+	 * Sends a numbered input to the system.
 	 *
 	 * Inputing is usually done by the 
 	 * {@link dev.joshpetit.wit.core.base.InputInterpreter}.
 	 * Once the system determines that it has the
 	 * necessary inputs to make a translation it will do
-	 * so with its {@link TypingSystem#config}.
+	 * so with its {@link TypingSystem#config} and send
+	 * the corresponding command to the
+	 * {@link TypingSystem#context}.
+	 * @param key The number to send to the system.
 	 */
 	public abstract void input(int key);
 
@@ -64,7 +70,7 @@ public abstract class TypingSystem<T extends Commandable> {
 	/**
 	 * Changes the current {@link TypingSystem#context}.
 	 */
-	public void setContext( T context) {
+	public void setContext(T context) {
 		this.context = context;
 	}
 
@@ -76,7 +82,7 @@ public abstract class TypingSystem<T extends Commandable> {
 
 	/**
 	 * Returns the current {@link TypingSystem#context} in which
-	 * this system is inputting into.
+	 * this system is inputting commands to.
 	 */
 	public T getContext() {
 		return this.context;
